41bc4d92deb17b9c287810a9a184e3f74b35f9fd
EdwinRozario <rozarioed@gmail.com>
2014-02-16 00:46:10 +1300

    mailtesting 2

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,7 +2,10 @@ class ContactMailer < ActionMailer::Base
   default from: "savessw@gmail.com"
 
   def contact_mail(from, message)
-  	p "*************************************"
+    p "********************************************"
+  	p ActionMailer::Base.smtp_settings[:user_name]
+  	p ActionMailer::Base.smtp_settings[:password]
+
     @message = message
     mail(:to => "rozarioed@gmail.com", :subject => "edwinrozario.herokuapp")
   end
