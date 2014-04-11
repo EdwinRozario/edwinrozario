bd065e0dcaa6b7612756559bd3d28ed860a06283
EdwinRozario <rozarioed@gmail.com>
2014-04-12 03:28:46 +1200

    Dont know

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module RailsBootstrap
   class Application < Rails::Application
     config.assets.paths << Rails.root.join("vendor", "assets", "fonts")
-    config.assets.initialize_on_precompile = true
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
