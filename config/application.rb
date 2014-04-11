cf502f944bf0ae07363e09ce98fbf0c9627fcecc
EdwinRozario <rozarioed@gmail.com>
2014-04-12 03:25:09 +1200

    Change pre complile to application folder

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module RailsBootstrap
   class Application < Rails::Application
     config.assets.paths << Rails.root.join("vendor", "assets", "fonts")
+    config.assets.initialize_on_precompile = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
