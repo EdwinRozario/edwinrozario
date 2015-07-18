efe5bdb8f97353b825de6d3ce2375b2eaf2e5942
EdwinRozario <rozarioed@gmail.com>
2015-07-18 16:05:08 +1200

    Changes for mailer new mailer for external use

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,13 @@
 class ContactMailer < ActionMailer::Base
   default from: "edwin@edwinrozario.me"
 
+  def external_mail(name, from, message, phone)
+    @message = message
+    @from = from
+    @phone = phone
+    mail(:to => "sr@rowlandwoodslegal.org", :subject => "rowlandwoodslegal.org [#{name}]")
+  end
+
   def contact_mail(name, from, message)
     @message = message
     @from = from
