87302bd1f9e0b8476779b11ad0abbbdb009df643
EdwinRozario <rozarioed@gmail.com>
2015-07-18 16:11:17 +1200

    fixing syntax

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -24,7 +24,7 @@ class HomeController < ApplicationController
     if params["external"]
       ContactMailer.external_mail(params["name"], params["email"], params["message"], params["phone"]).deliver
       redirect_to root_path      
-      render :json => {"status": "success"}
+      render :json => {status: "success"}
     else
       ContactMailer.contact_mail(params["name"], params["email"], params["message"]).deliver
       redirect_to root_path      
